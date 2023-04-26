418198bdda2f4683d5d6fbab645cdf065bbe84cb
errayn-hub <128374386+errayn-hub@users.noreply.github.com>
2023-04-26 12:51:32 +0300

    Initial Commit

---
+++ ButtonStyleView/ButtonStyleViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonStyleViewApp.swift
+//  ButtonStyleView
+//
+//  Created by ErRayn on 26.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ButtonStyleViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
